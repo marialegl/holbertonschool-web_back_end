31056b88932644bb20a002ca8a921feb569a56a7
marialegl <psico.marialegl@gmail.com>
2024-07-22 21:39:07 -0500

    Update Tasks 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -4,7 +4,7 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
-  
+
   get name() {
     return this._name;
   }
@@ -15,24 +15,24 @@ export default class HolbertonCourse {
     }
     this._name = newName;
   }
-  
+
   get length() {
     return this._length;
   }
-  
+
   set length(newLength) {
     if (typeof newLength !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = newLength;
   }
-  
+
   get students() {
     return this._students;
   }
-  
+
   set students(newStudents) {
-    if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
+    if (!Array.isArray(newStudents) || !newStudents.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = newStudents;
