2ff8839911d983cf3e5408e419d60aedbd86231a
marialegl <psico.marialegl@gmail.com>
2024-07-28 20:58:46 -0500

    Update

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -7,6 +7,6 @@ export default function handleResponseFromAPI(promise) {
     };
   }).catch(() => {
     console.log('Got a response from the API');
-    return new Error(' ');
+    return new Error();
   });
 }
