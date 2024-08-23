f8bb10c2be01ae00ce74ebb902a1da1eacd104aa
marialegl <psico.marialegl@gmail.com>
2024-08-22 22:55:10 -0500

    update Tasks 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
+"""
+Let's execute multiple coroutines at the same time with async
+"""
 
 
 import asyncio
 from typing import List
-from tasks import task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Executes task_wait_random n times concurrently and
-    returns the results in ascending order."""
+    """
+    Executes task_wait_random n times concurrently and
+    returns the results in ascending order.
+
+    Parameters:
+    n: int
+    max_dalay: int
+
+    Returns:
+    return the list of all the delays (float values)
+    """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
 
