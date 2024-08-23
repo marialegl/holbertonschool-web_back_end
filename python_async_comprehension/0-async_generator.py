3949a3afa40ee07eac694bcd5eb889555a81b73d
marialegl <psico.marialegl@gmail.com>
2024-08-22 22:24:19 -0500

    Tasks 0 Update

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-
+"""
+Async Generator
+"""
 
 import random
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None]:
     """Definition of the coroutine"""
     for _ in range(10):
         """Loop that repeats 10 times"""
